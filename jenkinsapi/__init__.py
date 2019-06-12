8a60aa14ca9f43bcdf87b3fa66f9ad48a715541a
Jiri Polansky <jiri.polansky@twisto.cz>
2019-06-12 15:18:38 +0200

    fix             for i in self._data['changeSets']:

--- jenkinsapi/__init__.py
+++ jenkinsapi/__init__.py
@@ -67,9 +67,9 @@ __all__ = [
 ]
 __docformat__ = "epytext"
 # In case of jenkinsapi is not installed in 'develop' mode
-__version__ = '99.99.99'
-try:
-    import pkg_resources
-    __version__ = pkg_resources.working_set.by_key['jenkinsapi'].version
-except (ImportError, KeyError):
-    pass
+__version__ = '0.3.9.twisto.1'
+# try:
+#     import pkg_resources
+#     __version__ = pkg_resources.working_set.by_key['jenkinsapi'].version
+# except (ImportError, KeyError):
+#     pass
