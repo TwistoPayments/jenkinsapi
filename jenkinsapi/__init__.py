0d3fb884c71c2da4621ba0df8853490f25bb3b52
Jiri Polansky <jiri.polansky@twisto.cz>
2019-06-12 14:52:35 +0200

    fix version

--- jenkinsapi/__init__.py
+++ jenkinsapi/__init__.py
@@ -67,7 +67,7 @@ __all__ = [
 ]
 __docformat__ = "epytext"
 # In case of jenkinsapi is not installed in 'develop' mode
-__version__ = '99.99.99'
+__version__ = '0.3.9.twisto.1'
 try:
     import pkg_resources
     __version__ = pkg_resources.working_set.by_key['jenkinsapi'].version
